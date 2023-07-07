afe2f35c4370595ddcc38273352e454f5a572691
offensivedroid <138177031+offensivedroid@users.noreply.github.com>
2023-07-07 11:34:51 +0800

    Debugger
    Added a python program with error that you can debug.

---
+++ Debugging/debugger.py
@@ -0,0 +1,12 @@
+def divide_numbers(a, b):
+    result = a / b
+    return result
+
+def main():
+    num1 = 10
+    num2 = 0
+    breakpoint()
+    result = divide_numbers(num1, num2)
+    print("Result: ", result)
+
+main()
